1b9aa978dc725fc38a9ab6dd255df5414a7c2fb3
Yaaqoub Charkouk <yaaqoubcourses@gmail.com>
2026-05-17 21:31:42 +0100

    feat: configure WordPress site installation and user creation using WP-CLI in Docker container

--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -14,6 +14,29 @@ if [ ! -f "wp-config.php" ]; then
 
 fi
 
+while ! mysqladmin ping -h"$MYSQL_HOST" --silent; do
+    sleep 2
+done
+
+if ! wp core is-installed --allow-root; then
+
+    wp core install \
+        --url="$DOMAIN_NAME" \
+        --title="$WP_TITLE" \
+        --admin_user="$WP_ADMIN_USER" \
+        --admin_password="$WP_ADMIN_PASSWORD" \
+        --admin_email="$WP_ADMIN_EMAIL" \
+        --skip-email \
+        --allow-root
+
+    wp user create \
+        "$WP_USER" \
+        "$WP_USER_EMAIL" \
+        --user_pass="$WP_USER_PASSWORD" \
+        --role=author \
+        --allow-root
+fi
+
 exec php-fpm8.2 -F
 
 # $MYSQL_HOST tells wordpress where does the database server lives (service name : mariadb)
